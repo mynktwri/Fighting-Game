9fef60367c9fb25534d5a953c8876c71cc14f692
Mayank Tiwari <mayank@rockinfamily.com>
2015-10-14 23:08:20 -0700

    First Commit

---
+++ FightingGame/Assets/Editor/Entity.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Entity : MonoBehaviour {
+	public float life = 10f;
+	public float defenseMod = 0f;
+    private float maxSpeed = 10f;
+    private float jumpForce = 400f;
+    private float crouchSpeed = 0.45f;
+    private bool AirControl = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+}
