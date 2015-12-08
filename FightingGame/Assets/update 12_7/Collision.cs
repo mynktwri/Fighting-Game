7ef85239ec3e63d2f418d37c05e5f1eb3df15373
Anthony <adspano91@gmail.com>
2015-12-07 20:55:42 -0800

    ihhbjkjn
    iouhuyhghjkbnm

---
+++ FightingGame/Assets/update 12_7/Collision.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collision : MonoBehaviour {
+
+	private GameObject player1;
+	private GameObject player2;
+	private GameObject punchBox;
+	private BoxCollider2D bc;
+
+	public EnemyHealth enemyHealth;
+	public PlayerHealth playerHealth;
+
+	// Use this for initialization
+	void Start () {
+		bc = GetComponent<BoxCollider2D>();
+	}
+
+	void Awake () {
+	
+		player1 = GameObject.FindGameObjectWithTag("Player");
+		player2 = GameObject.FindGameObjectWithTag("Enemy");
+		enemyHealth = player2.GetComponent<EnemyHealth>();
+		punchBox = GameObject.FindGameObjectWithTag("Punch");
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (GameObject.FindGameObjectWithTag("Punch") == player2)
+		enemyHealth.p2Health = enemyHealth.p2Health - 20;
+
+		//if (GameObject.FindGameObjectWithTag("Punch") == player1)
+			//playerHealth.p1Health = playerHealth.p1Health - 20;
+	} 
+
+}
---
+++ FightingGame/Assets/update 12_7/PlayerAction.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets._2D;
+
+
+public class PlayerAction : MonoBehaviour {
+	private Animator anim_action;
+	//private Rigidbody2D rb;
+	//private PlatformerCharacter2D pc;
+	//private Vector2 point;
+	private BoxCollider2D bc;
+
+	public EnemyHealth enemyHealth;
+	public PlayerHealth playerHealth;
+	public GameObject player1;
+	public GameObject player2;
+
+	//private bool isHit = false;
+
+	BoxCollider2D punchBox;
+	//float radius = 1.0f;
+
+
+
+
+    // Use this for initialization
+    void Start () 
+	{
+		//pc = GetComponent<PlatformerCharacter2D>();
+		//rb = GetComponent<Rigidbody2D>();
+		bc = GetComponent<BoxCollider2D>();
+		anim_action = GetComponent<Animator>();
+
+	}
+
+    void Awake ()
+    {
+		player1 = GameObject.FindGameObjectWithTag("Player");
+        player2 = GameObject.FindGameObjectWithTag("Enemy");
+		punchBox = bc;
+        enemyHealth = player2.GetComponent<EnemyHealth>();
+		//punchBox = GameObject.FindGameObjectWithTag("Punch");
+    }
+
+	void Update()
+	{
+		// IgnoreCollider
+		//Physics2D.IgnoreCollision(player1.GetComponent<Collider2D>(), player2.GetComponent<Collider2D>()); // Remove bumping damage
+
+		Punch();
+
+		punchBox.gameObject.SetActive(false);
+	}
+
+
+
+	void Punch () //make a collision object with collision script attached
+	{
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+			//isHit = true;
+
+			//anim_action.SetBool("Punch", isHit);
+            anim_action.SetTrigger("Punch");
+			punchBox.gameObject.SetActive(true);
+
+			         
+			//isHit = false;
+			punchBox.gameObject.SetActive(false);
+
+        }
+
+	}
+
+
+}
